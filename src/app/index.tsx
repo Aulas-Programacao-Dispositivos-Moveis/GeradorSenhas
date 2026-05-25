624bd0514058319c7cc0c5afa0f019808f38a358
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-05-25 11:28:02 -0300

    feat: criando tela de geração de senha

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,11 @@
-import { Text } from "react-native";
+import Slider from '@react-native-community/slider';
+import { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { colors } from "../const/Colors";
 
 export default function Index() {
+  const [ passwordLength, setPasswordLength ] = useState(8);
   return (
     <SafeAreaView
       style={{
@@ -12,7 +15,54 @@ export default function Index() {
         backgroundColor: colors.gray
       }}
     >
-      <Text>Gerador de senhas</Text>
+      <Image
+        source={require("../../assets/logo.png")}
+        style={styles.icone}
+      />
+      <Text
+        style={styles.texto}
+      >{passwordLength} caracteres</Text>
+
+      <Slider
+        style={{ width: 250, height: 40 }}
+        minimumValue={8}
+        maximumValue={24}
+        step={1}
+        onValueChange={setPasswordLength}
+        value={passwordLength}
+        minimumTrackTintColor={colors.blue}
+        maximumTrackTintColor="#000000"
+        thumbTintColor={colors.blue}
+        thumbSize={16}
+      />
+
+      <TouchableOpacity 
+        style={ styles.botao }
+        onPress={() => alert(`Passwd len. ${passwordLength}`)}  
+      >
+        <Text style={ styles.textoBotao }>Gerar Senha</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  icone: {
+    width: 250,
+    height: 250,
+    marginBottom: 40
+  },
+  texto: {
+    fontSize: 20
+  },
+  botao: {
+    width: 250,
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: colors.blue
+  },
+  textoBotao: {
+    color: "white",
+    textAlign: "center"
+  }
+});
